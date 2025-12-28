36edc86af0950ea7f18e42532afa91530475821c
Mohamad <mohamad@Mohamads-MacBook-Air.local>
2025-12-28 19:40:18 +0330

    added start command to welcome users

--- Megachat_tmb.py
+++ Megachat_tmb.py
@@ -4,10 +4,13 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Messa
 
 token = "..."
 
-
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام به ربات ما خوش آمدید.")
 
 def main():
     app = ApplicationBuilder().token(token).build()
+    
+    app.add_handler(CommandHandler("start", start_command))
 
 
     print ("Bot is running...")
