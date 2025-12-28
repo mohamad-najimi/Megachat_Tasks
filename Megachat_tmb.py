8ef264431e24002470502168918da70ae4826067
Mohamad <mohamad@Mohamads-MacBook-Air.local>
2025-12-28 19:42:08 +0330

    added message handler to reply to user messages

--- Megachat_tmb.py
+++ Megachat_tmb.py
@@ -7,11 +7,18 @@ token = "..."
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام به ربات ما خوش آمدید.")
 
+async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_text = update.message.text
+    if user_text == "سلام":
+        await update.message.reply_text("سلام")
+    else:
+        await update.message.reply_text(f"پیام شما: {user_text}")
+
 def main():
     app = ApplicationBuilder().token(token).build()
-    
-    app.add_handler(CommandHandler("start", start_command))
 
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(MessageHandler(filters.TEXT, handle_text))
 
     print ("Bot is running...")
     app.run_polling()
